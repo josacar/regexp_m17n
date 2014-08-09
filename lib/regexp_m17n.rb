83f68ad1bf8619239825f36af2f715830ba7050a
Jose Luis Salas <josacar@gmail.com>
2014-08-09 18:12:12 +0200

    Conversion with encodings and monkey patch
    The monkey patch gets rid of Exception raised when invoking `non_empty?`
    with the `'.'.encode` method call

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,25 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    String.class_eval do
+      def encode(string)
+        ec = Encoding::Converter.new(self.encoding, string)
+        ec.convert(self)
+      rescue Encoding::ConverterNotFoundError
+        self.force_encoding(string)
+      end
+    end
+
+    regexp = '^.+$'
+    encoding = Encoding.find(str.encoding)
+
+    string = if encoding.dummy?
+      str.encode('UTF-8')
+    else
+      regexp = regexp.encode(str.encoding)
+      str
+    end
+    regexp = Regexp.new(regexp)
+
+    regexp =~ string
   end
 end
